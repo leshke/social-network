b4024d17f33d6a5c89d5889b766d141cd80bcf00
leshke <32702166+leshke@users.noreply.github.com>
2020-11-26 17:25:08 +0200

    Updates

---
+++ precache-manifest.4c453798e5886f410b80468486654aa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64df9efa07ecde06eb45dc07d242d52e",
+    "url": "/leshke/social-network/index.html"
+  },
+  {
+    "revision": "199450456c54703a0465",
+    "url": "/leshke/social-network/static/css/main.4d9542da.chunk.css"
+  },
+  {
+    "revision": "502a7f8a2a438449fa26",
+    "url": "/leshke/social-network/static/js/2.a48dccfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/leshke/social-network/static/js/2.a48dccfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199450456c54703a0465",
+    "url": "/leshke/social-network/static/js/main.91bcb2e3.chunk.js"
+  },
+  {
+    "revision": "a24ff1a795ab198bdba1",
+    "url": "/leshke/social-network/static/js/runtime-main.1214cab4.js"
+  },
+  {
+    "revision": "bbc9d3efe58e34d178f79290b3ab4225",
+    "url": "/leshke/social-network/static/media/avatar.bbc9d3ef.png"
+  }
+]);
